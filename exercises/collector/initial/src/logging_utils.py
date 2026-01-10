8df33df6dc6972f760a213e6c97df7653ff3ebdb
kajtek <kajtek0123@gmail.com>
2026-01-10 20:53:01 +0100

    chapter11

--- exercises/collector/initial/src/logging_utils.py
+++ exercises/collector/initial/src/logging_utils.py
@@ -1,6 +1,7 @@
 from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
 from opentelemetry.sdk._logs.export import ConsoleLogExporter, SimpleLogRecordProcessor
 from opentelemetry.sdk.resources import Resource
+from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
 
 logger_provider = LoggerProvider(
     resource=Resource.create(
@@ -9,6 +10,9 @@ logger_provider = LoggerProvider(
         }
     ),
 )
-
-logger_provider.add_log_record_processor(SimpleLogRecordProcessor(exporter=ConsoleLogExporter()))
+log_exporter = OTLPLogExporter(insecure=True)
+record_processor = SimpleLogRecordProcessor(
+    exporter=log_exporter
+)
+logger_provider.add_log_record_processor(record_processor)
 handler = LoggingHandler(logger_provider=logger_provider)
